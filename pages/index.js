bbd5e1007a15e3cf179e6ec818040a165555eb1f
KIMURA MASAYUKI <>
2023-10-11 12:13:48 +0900

    ✨ カテゴリ一覧を表示

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,38 @@ import styles from '../styles/css/Home.module.css'
 import Link from "next/link";
 import { client } from "../libs/client";
 
-export default function Home({ blog }) {
+export default function Home({ blog ,category}) {
   return (
-    <div className={styles.container}>
-      <ul>
-        {blog.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className={styles.container}>
+        <ul>
+          {category.map((category) => (
+            <li key={category.id}>
+              <Link href={`/category/${category.id}`}>{category.name}</Link>
+            </li>
+          ))}
+        </ul>
+        <ul>
+          {blog.map((blog) => (
+            <li key={blog.id}>
+              <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      </>
   );
 }
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "blog" });
+  const categoryData = await client.get({ endpoint: "category" });
 
   return {
     props: {
       blog: data.contents,
+      category: categoryData.contents,
     },
   };
 };
