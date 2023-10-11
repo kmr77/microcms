f3a7ec20a56d111d08ee22466eec2202103c6e6c
KIMURA MASAYUKI <>
2023-10-11 10:24:10 +0900

    ♻️ スタイル編集

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { client } from "../libs/client";
 
 export default function Home({ blog }) {
   return (
-    <div>
+    <div className={styles.container}>
       <ul>
         {blog.map((blog) => (
           <li key={blog.id}>
