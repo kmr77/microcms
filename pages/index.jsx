6d4b6158df10028d220316e14f8fd78dacbd91e4
KIMURA MASAYUKI <>
2023-10-12 15:43:57 +0900

    ♻️ 記事リストを表示準備

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,37 +1,63 @@
-import styles from '../styles/css/Home.module.css'
+import styles from '/components/css/Home.module.css'
 import Link from "next/link";
 import { client } from "/libs/client";
 import { Header } from '/components/Header';
 import { Headline } from '/components/Headline';
 import { MainVisual } from '/components/MainVisual';
+import Breadcrumb from 'components/Breadcrumb';
+import TopContents from 'components/TopContents';
 
 export default function Home({ blog ,category, tag}) {
   return (
     <>
     <Header  title="とっぷ" discription="インデックスDisc"/>
     <Headline/>
-    <MainVisual />
-      <div>
-        <ul>
-          {tag.map((tag) => (
-            <li key={tag.id}>
-              <Link href={`/tag/${tag.id}`}>{tag.name}</Link>
-            </li>
-          ))}
-        </ul>
-        <ul>
-          {category.map((category) => (
-            <li key={category.id}>
-              <Link href={`/category/${category.id}`}>{category.name}</Link>
-            </li>
-          ))}
-        </ul>
-        <ul>
-          {blog.map((blog) => (
-            <li key={blog.id}>
-              <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
-            </li>
-          ))}
+      <div className={styles.content}>
+        <MainVisual />
+        <Breadcrumb />
+        <div className={`${styles.top_section} ${styles.arrival}`}>
+          <div className={styles.inner}>
+            <TopContents />
+          </div>
+        </div>
+        <div>
+          <ul>
+            {tag.map((tag) => (
+              <li key={tag.id}>
+                <Link href={`/tag/${tag.id}`}>{tag.name}</Link>
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {category.map((category) => (
+              <li key={category.id}>
+                <Link href={`/category/${category.id}`}>{category.name}</Link>
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {blog.map((blog) => (
+              <li key={blog.id}>
+                <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <ul className={`${styles.article_list} ${styles.arrival} ${styles.top}`}>
+        <li className={styles.articles_item}>
+          <a className={styles.articles_img} href={`/`}>
+          </a>
+          <div className={styles.articles_content}>
+            <div className={styles.articles_author}></div>
+            <p className={styles.articles_date}></p>
+            <a className={styles.articles_link} href={`/`}>
+              <h3 className={styles.articles_title}></h3>
+            </a>
+            <p className={styles.articles_category}>
+              <span className={styles.articles_category_item}></span>
+            </p>
+          </div>
+        </li>
         </ul>
       </div>
       </>
