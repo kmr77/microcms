1684e1ad483397fc52339aef581b47ecdae634dd
KIMURA MASAYUKI <>
2023-10-12 15:42:49 +0900

    ✨ MainVisual作成

--- components/MainVisual/index.jsx
+++ components/MainVisual/index.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export function MainVisual(props) {
   return (
     <>
-        <div className={styles.content}>
+        
         <div className={styles.mv_area}>
         <div className={styles.mv_area_inner}>
             <h1 className={styles.mv_area_title}><span><Image src="img/mv_logo.svg" alt="W&M journal" width={425} height={167}  /></span></h1>
@@ -17,7 +17,6 @@ export function MainVisual(props) {
         <div className={styles.mv_under}>
         <div className={styles.inner}>
             <p className={styles.mv_under_txt}>W&Mジャーナルでは、ウィークリーマンション・マンスリーマンションのお役立ち情報から、<br className={styles.tabonly} /><br className={styles.sponly} />住まいに関するアイデア・ヒントを発信中！</p>
-        </div>
         </div>
         </div>
     </>
